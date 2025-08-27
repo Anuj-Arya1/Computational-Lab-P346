1c43480360f8dcd7f1d6efdb7c7e603ba1331e8d
Anuj-Arya1 <anuj.arya@niser.ac.in>
2025-08-27 22:18:14 +0530

    hey

--- Codes/assign_04.py
+++ Codes/assign_04.py
@@ -9,13 +9,12 @@ o2 = Matrix_Operation()
 
 A = o2.read_matrix('DATA/Assign_04/Input_files/asgn_04_A_matrix')
 b = o2.read_matrix('DATA/Assign_04/Input_files/asgn_04_b')
-# print(o1.Jacobi_iterative(A,b))
 L,L_t = o1.choleski_decomposition(A)
-print(o1.cholesky_forwd_back(A,b))
-# print(L)
-# print(L_t)
-# print(o2.matrix_multiply(L,L_t))
-
+print("Solution of linear equation:",o1.cholesky_forwd_back(A,b))
+print(L)
+print(L_t)
+print(o2.matrix_multiply(L,L_t))
+print(o1.Jacobi_iterative(A,b))
 # OUTPUT 
 # Question 1
 # L = [[2.0, 0, 0, 0], [0.5, 1.6583123951777, 0, 0], [0.5, -0.753778361444409, 1.087114613009218, 0], [0.5, 0.45226701686664544, 0.08362420100070905, 1.2403473458920844]]
