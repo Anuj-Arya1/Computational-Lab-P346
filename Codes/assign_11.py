20e4a766d9fae9ce7a8f32edbfe54c7960d5ba6c
Anuj-Arya1 <anuj.arya@niser.ac.in>
2025-10-16 21:31:45 +0530

    assign11_update

--- Codes/assign_11.py
+++ Codes/assign_11.py
@@ -1,7 +1,7 @@
 
 import math
 from anuj_library import *
-o1 = Gauss_Jordon_Elimination()
+o1 = Integration()
 
 def f(x):
     return 1/x
@@ -13,8 +13,8 @@ def h(x):
 
 # MID POINT 
 # N = 289 for function f and N = 610 for function g
-print("For function f (by method midpoint):    ",o1.midpoint_integral(1,2,289,f))
-print("For function g (by method midpoint):    ",o1.midpoint_integral(0,math.pi/2,610,g)) 
+print("For function f (by method midpoint):    ",o1.midpoint_integral(1,2,(2-1)/289,f))
+print("For function g (by method midpoint):    ",o1.midpoint_integral(0,math.pi/2,(math.pi/2)/610,g)) 
 
 # SYMPSON 
 # N = 20 for function f and N = 22 for function g
@@ -28,8 +28,8 @@ print('Integration value for funtion h using Monte Carlo: ',o1.Monte_carlo(-1,1,
 
 # OUTPUT
 
-# For function f (by method midpoint):     0.6931468064035272
-# For function g (by method midpoint):     0.5707970370864707
+# For function f (by method midpoint):     0.693146806403525
+# For function g (by method midpoint):     0.5707970370864733
 
 # For function f (by method sympson):     0.6931473746651162
 # For function g (by method sympson):     0.570796987316687
