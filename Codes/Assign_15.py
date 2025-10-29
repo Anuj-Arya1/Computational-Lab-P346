69b04efd5b1b61128af0b50c3afdc97301651a59
Anuj-Arya1 <anuj.arya@niser.ac.in>
2025-10-30 00:28:05 +0530

    assign15update

--- Codes/Assign_15.py
+++ Codes/Assign_15.py
@@ -23,53 +23,71 @@ def BVP(x,gh,gl,t,tf,f_x,f_v):
     g = gl + (((gh - gl)*(200 - T2[-1]))/(T1[-1] - T2[-1]))
     x3,T3,z3 = o1.RK4_DSHO(x,g,t,tf,0.1,f_x,f_v)
     return x1,T1,z1,x2,T2,z2,x3,T3,z3
-# a,b,c,d,e,f,g,h,i = BVP(40,10,20,0,10,f_x,f_v)
+a,b,c,d,e,f,g,h,i = BVP(40,10,20,0,10,f_x,f_v)
+
+# x at which T = 100
+for i in range(len(h)-1):
+    if h[i]<=100 and h[i+1]>100:
+        print('Temperature',h[i],'at position',g[i])
+        print('Temperature',h[i+1],'at position',g[i+1])
+        break
+
 # print(h[-1])
-# # PLOTS
-# plt.plot(a,b,color='red',label='Curve with gh = 10')
-# plt.plot(d,e,color='blue',label='Curve with gl = 20')
-# plt.plot(g,h,color='black',label='Final Curve')
-# plt.xlabel('Time (t)')
-# plt.ylabel('Temperature (T)')
-# plt.legend()
-# plt.show()
+# PLOTS
+plt.plot(a,b,color='red',label='Curve with gh = 10')
+plt.plot(d,e,color='blue',label='Curve with gl = 20')
+plt.plot(g,h,color='black',label='Final Curve')
+plt.xlabel('Time (t)')
+plt.ylabel('Temperature (T)')
+plt.legend()
+plt.show()
+
+# OUTPUT of question 1
+# Temperature 98.78432481771769 at position 4.4
+# Temperature 100.25272383202632 at position 4.5
 
 # question 2
-def ux0(x):
+def Yx0(x):
     if abs(1-x)< 10**(-6):
         return 300
     else:
         return 0
-# L=X,T = Y
-def matrix_vector_mult(A, V):
-    result = [0 for i in range(len(A))]
-    for i in range(len(A)):
-        for j in range(len(A[0])):
-            result[i] += A[i][j]*V[j]
-    return result
-def heat_eqn(ny,nx,x0,y0,X,Y):
+# L=X,T =Y
+
+def heat_eqn(nt,nx,x0,X,t0,T):
     # X is length of rod
     dx = (X-x0)/(nx-1)
-    dy = (Y-y0)/(ny-1)
-    a= dy/(dx)**2
+    dt = (T-t0)/(nt-1)
+    a= dt/(dx)**2
     V0 = []
+    T0 = []
+    X0 = []
     for i in range(nx):
-        V0.append(ux0(x0+i*dx))
-    A = [[0 for i in range(nx)] for i in range(ny)]  # A = ny x nx matrix
-    for j in range(ny):
+        X0.append(x0 + i*dx)
+    for t in range(nt):
+        T0.append(t0 + t*dt)
+    for i in range(nx):
+        V0.append([Yx0(x0+i*dx)])
+    A = [[0 for i in range(nx)] for i in range(nx)]  # A = nx x nx matrix
+    res = [V0]
+    for i in range(nx):
         for j in range(nx):
             if  i==j :
-                A[j][j] = 1 - 2*a
+                A[i][j] = 1 - 2*a
             if i == j-1 or i == j+1:
-                A[j][j-1] = a
-                A[j][j+1] = a
-    V = []
-    for k in range(1,ny):
-        V1 = o2.matrix_multiply(A,V0)
-        V.append(V1)
-    return V,dx
+                A[i][j] = a
+    for k in range(0,nt):
+        rr = o2.matrix_multiply(A,res[k])
+        res.append(rr)
+    return res, T0, X0
 
-V,dx = heat_eqn(50,20,0,0,10,5)
 # PLOTS
-
+result, T0, X0 = heat_eqn(2000,21,0,2,0,2)
+time_int = [0,20,50,100,500,1000,1999]
+for i in time_int:
+    plt.plot(X0,result[i],label=f'Time = {T0[i]:.2f} s')
+plt.xlabel('Length of rod (x)')
+plt.ylabel('Temperature (T)')
+plt.legend()
+plt.show()
 
